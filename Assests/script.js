a47c8afdb741fe8763068d745b5bbf3e1fcdab6c
brooke-delgado <83773208+brooke-delgado@users.noreply.github.com>
2021-08-20 22:45:27 -0500

    changed some html still can't get 1st quiesiton to appear, decided to take questions, choices and answer out of 1 array

--- Assests/script.js
+++ Assests/script.js
@@ -1,4 +1,4 @@
-//Variables- get a handle
+//Variables
 var showCountdown = document.getElementById("timer");
 var questionPrompt = document.getElementById("question.prompt");
 var next = document.getElementById("next");
@@ -10,51 +10,71 @@ var answerAValue = document.getElementById("pick-A");
 var answerBValue = document.getElementById("pick-B");
 var answerCValue = document.getElementById("pick-C");
 var answerDValue = document.getElementById("pick-D");
+var startButtton = document.querySelector("#Start");
+var countDown = document.querySelector(".timer");
+var score = 0;
+var currentQuestion = 0;
 
-//Start Button
-var startBtn = document.querySelector("#Start");
+startButtton.addEventListener("click", countDown);
 
 //Timer
 var counter = 60;
 var countDown = setInterval(function () {
   showCountdown.textContent = counter;
   counter--;
+  if (counter < 0) {
+    timer.textContent = "SORRY TIME'S UP!";
+    clearInterval(countDown);
+  }
 }, 1000);
-
-
-//Populate first question
-if (startBtn) {
-  questionPrompt.textContent = listQuestions[0];
-}
 
 //// QUIZ ARRAY /////
 
 var listQuestions = [
   "Which of the following is not JavaScript Data Types?",
-  //choices: ["A('Undefined')", "B('Number')", "C('Boolean')", "D('Float')"],
-  //Answer: "D('Float')",
   "Which company developed JavaScript?",
-  //choices: [
-  //"A('Netscape')",
-  // "B('Bell Labs')",
-  // "C('Sun Microsystems')",
-  // "D('IBM')",
-  //Answer: "A('Netscape'')",
   "Inside which HTML element do we put the JavaScript?",
-  //choices: ["A('<script>')", "B('<head>')", "C('<meta>')", "D('<style>')"],
-  //Answer: "A('<script>')",
   "Why do JavaScript and Java have similar name?",
-  //choices: [
-  // "A('JavaScript is a stripped-down version of Java')",
-  // "B('JavaScript's syntax is loosely based on Java's')",
-  // "C('They both originated on the island of Java')",
-  //  "D('None of the above')",
-  //Answer: "B('JavaScript's syntax is loosely based on Java's')",
   "What is the original name of JavaScript?",
-  //choices: [
-  //  "A('LiveScript')",
-  // "B('EScript')",
-  // "C('Mocha)",
-  // "D('JavaScript')",
-  //Answer: "C('Mocha')",
 ];
+
+//Show first question
+startButtton.addEventListener("click", nextQuestion);
+
+//show answer choices for first question
+if (questionPrompt.textContent === questions[0]) {
+  //Show options for first question
+  answerA.textContent = "Undefined";
+  answerB.textContent = "Number";
+  answerC.textContent = "Boolean";
+  answerD.textContent = "Float";
+  //answer = D
+}
+
+//Show options for second question
+answerA.textContent = "Netscape";
+answerB.textContent = "Bell Labs";
+answerC.textContent = "Sun Microsystems";
+answerD.textContent = "IBM";
+//answer = A
+
+//Show options for third question
+answerA.textContent = "<script>";
+answerB.textContent = "<head>";
+answerC.textContent = "<meta>";
+answerD.textContent = "<style>";
+//Answer= A
+
+//Show options for fourth question
+answerA.textContent = "JavaScript is a stripped-down version of Java";
+answerB.textContent = "JavaScript's syntax is loosely based on Java's";
+answerC.textContent = "They both originated on the island of Java";
+answerD.textContent = "None of the above";
+//Answer= B
+
+//Show options for fifth question
+answerA.textContent = "LiveScript";
+answerB.textContent = "EScript";
+answerC.textContent = "Mocha";
+answerD.textContent = "JavaScript";
+//Answer= C
